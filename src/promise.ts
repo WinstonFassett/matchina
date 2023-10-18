16bba29a41c80da4d8dfa2040d92fa13819f7e43
Winston Fassett <winston.fassett@gmail.com>
2023-10-18 08:40:33 -0500

    promise tweaks, use onUpdate and state match

--- src/promise.ts
+++ src/promise.ts
@@ -1,13 +1,13 @@
 import { defineMachine } from "./machine";
-import { onTransition } from "./on-transition";
+import { onUpdate } from "./on-update";
 import { createStates } from "./states";
 
 export function createPromiseMachine<T, A, E extends Error = Error>(
   makePromise?: (...args: A[]) => Promise<T>,
-) {
+  ) {
   const states = createStates({
     Idle: undefined,
-    Pending: (...params: A[]) => ({ params }),
+    Pending: (...params: A[]) => params,
     Rejected: (error: E) => error,
     Resolved: (data: T) => data,
   });
@@ -20,16 +20,31 @@ export function createPromiseMachine<T, A, E extends Error = Error>(
     Resolved: { execute: "Pending" },
     Rejected: { execute: "Pending" },
   });
-  const machine = Machine.create(Machine.states.Idle());
+  const initialState = Machine.states.Idle()
+  const machine = Machine.create(initialState);
   if (makePromise) {
-    onTransition(machine, (t, ev, params) => {
-      if (ev === "execute") {
-        makePromise(...(params as any))
-          .then(machine.events.resolve)
-          .catch(machine.events.reject);
-      }
-      return t(ev, params);
-    });
+    const _makePromise = makePromise
+    function execute(params: any[]) {
+      const promise = _makePromise(...params);
+      promiseMachine.promise = promise;
+      promiseMachine.done = promise
+        .then(machine.events.resolve)
+        .catch(machine.events.reject);
+    }
+    onUpdate(machine, (commit, updater) => {
+      const after = updater(machine.getLast())
+      after.to.match({ 
+        Pending: execute, 
+        _(){}
+      })
+      commit(() => after)      
+    })
   }
-  return machine;
+  const promiseMachine = Object.assign(machine, {
+    promise: undefined as undefined | Promise<T>,
+    done: undefined as undefined | Promise<void>
+  });
+  return promiseMachine
+
+
 }
