ce3ca307b7ea161083131060ff6a68229a55f54d
Winston Fassett <winston.fassett@gmail.com>
2023-10-19 19:07:49 -0500

    promise usage

--- playground/promise.usage.ts
+++ playground/promise.usage.ts
@@ -2,36 +2,68 @@ import { Expand } from "../src/utility-types";
 import { createPromiseMachine } from "../src/promise";
 import { delay } from "../src/delay";
 
-const machine = createPromiseMachine((x: number) =>
-  delay(x).then(() => `slept for ${x}ms`),
-);
+async function promiseUsage () {
 
-type X = Expand<typeof machine.events>;
-const x: X = {} as X;
+  const machine = createPromiseMachine(async (x: number) => {
+    console.log('sleeping for', x)
+    await delay(x)
+    return `slept for ${x}ms`
+  });
+  
+  type X = Expand<typeof machine.events>;
+  const x: X = {} as X;
+  
+  const it = machine.getState().match({
+    Rejected: () => ({ kablamo: false }),
+    _: () => ({ kablamo: true }),
+  });
+  console.log(it)
+  
+  const checkState = () => console.log(machine.getState().match({  
+    Resolved: res => `DONE: ${res}`,
+    Rejected: err => `Error! ${err}`,
+    _: () => `Not yet: ${machine.getState().state}`
+  }));
+  // TODO: add this to promise
+  const reset = () => {
+    console.log('resetting')
+    machine.update(c => ({...c, to: machine.states.Idle() }))
+  }
+  checkState()
+  machine.events.execute(1000);
+  checkState()
+  const pendingPromise = machine.promise
+  if (pendingPromise !== machine.promise) {
+    console.log('Got valid result BUT promise changed!' )    
+  }
+  reset()
+  machine.events.execute(1)
+  const donePromise = machine.done
+  const beforeDoneState = machine.getState()
+  checkState()
+  console.log('rejecting')
+  machine.events.reject(new Error("error"));
+  checkState()
+  
+  await donePromise
+  const state = machine.getState()
+  if (machine.done === donePromise && state === beforeDoneState) {
+    console.log('changed', state.data)
+  } else {
+    console.log(`state changed from ${beforeDoneState.state} to ${state.state}`)
+  }
+  
 
-const it = machine.getState().match({
-  Rejected: () => ({ kablamo: false }),
-  _: () => ({ kablamo: true }),
-});
-console.log(it)
-
-const checkState = () => console.log(machine.getState().match({  
-  Resolved: res => `DONE: ${res}`,
-  Rejected: err => `Error! ${err}`,
-  _: () => `NOPE: ${machine.getState().state}`
-}));
-const reset = () => machine.update(c => ({...c, to: machine.states.Idle() }))
-
-machine.events.execute(1000);
-machine.events.reject(new Error("error"));
-checkState()
-
-reset()
-machine.send("execute", [1]);
-delay(2).then(checkState)
-.then(() => {
+  reset()
+  machine.send("execute", [1]);
+  await delay(2)
+  checkState()
   reset()
   machine.transitions.Idle.execute(2)
-  delay(2).then(checkState)
-})
+  await delay(1)
+  checkState()
+  await delay(1)
+  checkState()
+}
 
+promiseUsage()
