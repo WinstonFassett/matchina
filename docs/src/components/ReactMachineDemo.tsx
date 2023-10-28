60e15423232ecfe946f83d3e8b3c49152b9bf347
Winston Fassett <winston.fassett@gmail.com>
2023-10-28 18:26:25 -0500

    cp

--- docs/src/components/ReactMachineDemo.tsx
+++ docs/src/components/ReactMachineDemo.tsx
@@ -1,41 +1,65 @@
-import { useMemo } from "react";
 import { createPromiseMachine, withSubscribe } from "../../../src";
 import { useMachine } from "../../../src/extras/react";
 
-const slowlyAddTwoNumbers = (x: number, y: number) =>
-  new Promise((resolve) => setTimeout(() => resolve(x + y), 1000));
+const slowlyAddTwoNumbers = (
+  x: number,
+  y: number,
+  duration = 1000,
+  name = "unnamed",
+) => new Promise((resolve) => setTimeout(() => resolve(x + y), duration));
 
+const machine = withSubscribe(createPromiseMachine(slowlyAddTwoNumbers));
 
 export function ReactMachineDemo({}) {
+  const [state] = useMachine(machine);
 
-  const machine = useMemo(() => withSubscribe(
-    createPromiseMachine(
-      slowlyAddTwoNumbers
-    )
-  ), [])
+  return (
+    <div>
+      <div>
+        Action:{" "}
+        {machine.getState().match({
+          // render based on state
+          Idle: () => (
+            <span>
+              <button onClick={() => machine.send("execute", 1, 1)}>
+                Add 1+1
+              </button>{" "}
+              or{" "}
+              <button
+                onClick={() =>
+                  machine.event.execute(2, 2, 2000, "Test two plus two")
+                }
+              >
+                Add 2+2
+              </button>
+            </span>
+          ),
+          Pending: ([x, y, duration, name]) => (
+            <span>
+              Waiting {duration?.toString() ?? "default=1000"}ms to add {x} +{" "}
+              {y}
+              {!!name && `(aka ${name})`}
+            </span>
+          ),
+          _: () => (
+            <span>
+              Done! <button onClick={machine.reset}>Reset</button>
+            </span>
+          ),
+        })}
+      </div>
 
-  const [state] = useMachine(machine)
-
-  return <div>
-
-    <p>
-      Action: 
-
-      {state.match({ // render based on state
-        Idle: () => <button onClick={() => machine.send("execute", 1,1)}>Add 1+1</button>,
-        Pending: () => <span>Waiting 1000ms</span>,
-        _: () => <button onClick={machine.reset}>Reset</button>
-      })}
-
-    </p>
-
-    <pre>
-      {JSON.stringify({
-        "Current State Key": state.key,
-        "Current State Data": state.data,
-        "Last Change": machine.getChange()
-      }, null, 2)}
-    </pre>
-
-  </div>;
+      <pre>
+        {JSON.stringify(
+          {
+            "Current State Key": state.key,
+            "Current State Data": state.data,
+            "Last Change": machine.getChange(),
+          },
+          null,
+          2,
+        )}
+      </pre>
+    </div>
+  );
 }
