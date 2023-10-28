a4cb691b1f3896bd39021d21ed7fb7cea53f533a
Winston Fassett <winston.fassett@gmail.com>
2023-10-28 11:07:50 -0500

    sep react entry

--- build.config.ts
+++ build.config.ts
@@ -1,5 +1,19 @@
 import { defineBuildConfig } from 'unbuild'
 
 export default defineBuildConfig({
+  entries: [
+    {
+      input: 'src/index.ts',
+      builder: 'rollup'
+    },
+    {
+      input: 'src/extras/react.ts',
+      builder: 'rollup'
+    }
+  ],
+  declaration: true,
+  rollup: {
+    emitCJS: true,  
+  },
   externals: ['react', 'react-dom'],
 })
