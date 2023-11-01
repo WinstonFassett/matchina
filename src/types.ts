1dcb3d709c4dfbe7cbf125855a862baa4c6881da
Winston Fassett <winston.fassett@gmail.com>
2023-11-01 05:05:00 -0500

    util types

--- src/types.ts
+++ src/types.ts
@@ -7,6 +7,9 @@ export interface ChangeEvent<Type, From, To> {
   from: From;
   to: To;
 }
+export type AnyFunction = (...args: any[]) => unknown;
+export type Func<A extends any[], R> = (...args: A) => R;
+export type ArgsType<F extends Func<any, any>> = Parameters<F>;
 export type CreateFunc<T, P = any> = (...args: P[]) => T;
 export type SwapFunc<T> = (updater: (event: T) => T) => void;
 
@@ -39,4 +42,31 @@ export type FlatMemberUnionToIntersection<T> = TUnionToIntersection<
 
 export type Expand<T> = T extends infer O ? { [K in keyof O]: O[K] } : never;
 
+export type Simplify<T> = DrainOuterGeneric<{ [K in keyof T]: T[K] } & {}>
+
+export type DrainOuterGeneric<T> = [T] extends [unknown] ? T : never
+
+export type ExtractColumnType<DB, TB extends keyof DB, C> =
+  // Inline version of DrainOuterGeneric for performance reasons.
+  // Don't replace with DrainOuterGeneric!
+  [DB] extends [unknown]
+    ? {
+        [T in TB]: C extends keyof DB[T] ? DB[T][C] : never
+      }[TB]
+    : never
+export type DictionaryValues<Type> = Type[keyof Type];
+
+
+// export type Head<Type extends AnyArray> = Type["length"] extends 0 ? never : Type[0];
+
+export type NonNever<Type extends {}> = Pick<
+  Type,
+  { [Key in keyof Type]: Type[Key] extends never ? never : Key }[keyof Type]
+>;
+
+export type KeysOfUnion<ObjectType> = ObjectType extends unknown
+	? keyof ObjectType
+	: never;
+
 // #endregion
+
