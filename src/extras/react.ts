a59cec543b42b519cfc5b68172ceec8d1fb39cd3
Winston Fassett <winston.fassett@gmail.com>
2023-11-02 07:30:51 -0500

    decouple react from types

--- src/extras/react.ts
+++ src/extras/react.ts
@@ -1,12 +1,9 @@
 import React, { useCallback } from "react";
-import type { StateMachine, TransitionConfig } from "../machine-types";
-import type { StatesFactory } from "../states";
-import type { SubscribableMachine } from "./with-subscribe";
 
-export function useMachine<
-  States extends StatesFactory,
-  Transitions extends TransitionConfig<States>,
->(machine: SubscribableMachine<StateMachine<States, Transitions>>) {
+export function useMachine<T>(machine: {
+  subscribe: (listener: (value: T) => void) => () => void;
+  getState: () => T;
+}) {
   return [
     React.useSyncExternalStore(
       useCallback(machine.subscribe, [machine]),
