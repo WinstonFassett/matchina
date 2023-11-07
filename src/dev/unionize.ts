b7840c118dcb9e7fb0d19d7374c4afa78767037a
Winston Fassett <winston.fassett@gmail.com>
2023-11-07 10:16:12 -0600

    cp

--- src/dev/unionize.ts
+++ src/dev/unionize.ts
@@ -1,3 +1,4 @@
+
 export type Unionized<Record, TaggedRecord, TagProp extends string> = UnionMeta<
   Record,
   TaggedRecord
@@ -22,10 +23,113 @@ export interface MemberExtensions<Record, TaggedRecord> {
   // transform: Transform<Record, TaggedRecord[keyof TaggedRecord]>;
 }
 
+/*
+ * Create a tagged union from a record mapping tags to value types, along with associated
+ * variant constructors, type predicates and `match` function.
+ * @param record A record mapping tags to value types. The actual values of the record don't
+ * matter; 
+ * 
+ * Type below needs some work
+ * Assume each value is a function or object, and we will run it through FunctionFrom<T>
+ * And the UnionFactory should return a function with the parameters of that function
+ * and returning the tagged record for that key, enhanced with a data property that
+ * is the return value of the original function (or the plain value if it is not a function)
+ * 
+ * 
+ */
+// export type UnionFactory<Def, TagProp extends string = 'tag', ValProp extends string = 'data'> = {
+//   [Tag in keyof Def]: (...args: Parameters<FunctionFrom<Def[Tag]>>)
+//     =>       
+//       // TaggedRecord[keyof TaggedRecord] &
+//       { data: ReturnType<FunctionFrom<Def[Tag]>> }
+// };
+
+// export type UnionizedFactory<Record, TaggedRecord, TagProp extends string> = UnionExtensions<
+//   Record,
+//   TaggedRecord
+// > &
+//   Creators<Record, TaggedRecord, TagProp> &
+//   UnionExtensions<Record, TaggedRecord>;
+
+// Derives its TaggedRecord from the values OR return types of the values
+type UnionizedDataFactory<
+  FunctionOrValueRecord,
+  TagProp extends string = "tag",
+  ValProp extends string = "data",
+> = FactoryCreators<FunctionOrValueRecord, TagProp, ValProp>
+
+type UnionizedDataFactoryCompat<
+  FunctionOrValueRecord,
+  TagProp extends string = "tag",
+  ValProp extends string = "data",
+> = Unionized<
+  FactoryValueRecord<FunctionOrValueRecord>,
+  FactoryMemberRecord<FunctionOrValueRecord, TagProp, ValProp>,
+  TagProp
+>;
+
+type FactoryValueRecord<FunctionOrValueRecord> = {
+  [T in keyof FunctionOrValueRecord]: 
+    FunctionOrValueRecord[T] extends (...args: any[]) => any ? 
+      ReturnType<FunctionOrValueRecord[T]> :
+      FunctionOrValueRecord[T]
+};
+
+type FactoryMemberRecord<FunctionOrValueRecord, TagProp extends string, ValProp extends string> = {
+  [Tag in keyof FunctionOrValueRecord]:     
+    FactoryMember<FunctionOrValueRecord, Tag, TagProp, ValProp>
+};
+
+// Derives creators from the values OR return types of the values
+// If value is function, creator uses its params
+export type FactoryCreators<FunctionOrValueRecord, TagProp extends string, ValProp extends string> = {
+  [T in keyof FunctionOrValueRecord]: 
+    FactoryMemberCreate<FunctionOrValueRecord, T, TagProp, ValProp>
+};
+
+type FactoryMemberCreate<FunctionOrValueRecord, Tag extends keyof FunctionOrValueRecord, TagProp extends string, ValProp extends string> = 
+FunctionOrValueRecord[Tag] extends (...args: infer P) => infer R ? 
+  (...args: P) => FactoryMember<FunctionOrValueRecord, Tag, TagProp, ValProp> :
+  (value?: FunctionOrValueRecord[Tag]) => FactoryMember<FunctionOrValueRecord, Tag, TagProp, ValProp>
+
+type FactoryMember<
+  FunctionOrValueRecord,
+  Tag extends keyof FunctionOrValueRecord,
+  TagProp extends string,
+  ValProp extends string,
+> = (FunctionOrValueRecord[Tag] extends (...args: any[]) => any
+  ? { [_ in ValProp]: ReturnType<FunctionOrValueRecord[Tag]> }
+  : { [_ in ValProp]: FunctionOrValueRecord[Tag] }) & { [_ in TagProp]: Tag }; 
+
+//  Unionized<
+//   // here, FunctionOrValueRecord is a mapping to values OR parameterized creator functions
+//   // Needs to be mapped to a Record of union data types, i.e. the return types
+//   FunctionOrValueRecord, 
+//   // Here FunctionOrValueRecord needs to be mapped to a Record of tagged union member types
+//   SingleValueVariants<FunctionOrValueRecord, TagProp, ValProp>, 
+//   TagProp
+// >;
+
+type F = FunctionFrom<(x: number, y: string) =>{ a: string }>
+type P = Parameters<F>;
+const config = {
+  Object: { hello: 'world'},
+  Function: (x: number, y: string) => ({ x }),  
+} as const
+type C = typeof config
+type X = UnionizedDataFactory<C>;
+const x: X = {} as any;
+const y = x.Object({ hello: 'world' })
+const z = x.Function(1, '2')
+type ValueMap = FactoryValueRecord<C>
+type MemberMap = FactoryMemberRecord<C, 'tag', 'data'>
+
+
+
 export type FactoryConfig = Record<string, FactoryItemSpec>;
 export type FactoryItemSpec = ((...args: any[]) => any) | any;
 
-export type FunctionFrom<T> = T extends (...args: any[]) => any ? T : () => T;
+export type FunctionFrom<T> = T extends (...args: any[]) => any ? T : T extends undefined ? () => T : (value?:T) => T;
 
 export type TaggedRecordOf<U extends UnionMeta<any, any>> = U['_TaggedRecord'];
 export type TagsOf<U extends UnionMeta<any, any>> = U['_Tags'];
@@ -67,8 +171,14 @@ export type MultiValueVariants<Record extends MultiValueRec<TagProp>, TagProp ex
 export type SingleValueVariants<
   Record extends SingleValueRec,
   TagProp extends string,
-  ValProp extends string
-> = { [T in keyof Record]: { [_ in TagProp]: T } & { [_ in ValProp]: Record[T] } };
+  ValProp extends string,
+> = {
+  [T in keyof Record]: { 
+    [_ in TagProp]: T 
+  } & { 
+    [_ in ValProp]: Record[T] 
+  };
+};
 
 // Forbid usage of default property; reserved for pattern matching.
 export interface NoDefaultProp {
@@ -162,6 +272,6 @@ export type NoDefaultRec<Val> = {
  * type `T` for any `T`, but it's really just returning `undefined`. This white lie
  * allows convenient expression of the value types in the record you pass to `unionize`.
  */
-export const ofType = <T>() => (undefined as any) as T;
+// export const ofType = <T>() => (undefined as any) as T;
 
 // export default unionize;
