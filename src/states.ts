6f9fb6f35974c6826d6548f620f23e2869c543ea
Winston Fassett <winston.fassett@gmail.com>
2023-11-10 05:57:11 -0600

    refine

--- src/states.ts
+++ src/states.ts
@@ -1,26 +1,11 @@
-import { matchboxFactory, UnionSpec, Member } from "./matchbox";
+import { matchboxFactory, UnionSpec, Member, MemberCreate } from "./matchbox";
 
-export type StatesMatchboxFactory<StatesConfig extends UnionSpec> =
-  StateCreators<StatesConfig>;
+export type StatesMatchboxFactory<Specs extends UnionSpec> =
+  StateCreators<Specs>;
 
 export type StateCreators<Specs> = {
-  [T in keyof Specs]: StateCreate<Specs, T, "key", "data">;
+  [T in keyof Specs]: MemberCreate<Specs, T, "key", "data">;
 };
-
-export type StateCreate<
-  Specs,
-  Tag extends keyof Specs,
-  TagProp extends string,
-  DataProp extends string,
-> = Specs[Tag] extends (...args: infer P) => infer R
-  ? (...args: P) => Member<Specs, Tag, TagProp, DataProp>
-  : () // value?: Specs[Tag]
-    => Member<Specs, Tag, TagProp, DataProp>;
-
-// export type MatchboxFromStatesFactory<
-//   States extends StatesMatchboxFactory<any>,
-//   K extends keyof States = keyof States,
-// > = MatchboxFromFactory<States, K>;
 
 export function defineStates<Config extends UnionSpec>(config: Config) {
   return matchboxFactory(config, "key") as StatesMatchboxFactory<Config>;
