132d17366eb004699db27c63d51150c0871bc634
Winston Fassett <winston.fassett@gmail.com>
2023-11-10 08:43:27 -0600

    better teaser

--- docs/src/content/docs/code/teaser.ts
+++ docs/src/content/docs/code/teaser.ts
@@ -1,43 +1,34 @@
-import { defineStates, defineMachine } from "../src";
+import { defineStates, createMachine } from "../src";
 import { withEvents } from "../src/extras/with-events";
-
-type SomeResult = {
-  someResult: string
-}
-
-type SomeRequest = {
-  id: number
-}
-
-const states = defineStates({
-  Idle: undefined,
-  Pending: (req: SomeRequest) => req,
-  Rejected: (error: Error) => error,
-  Resolved: (data: SomeResult) => data,
-})
-
-const Machine = defineMachine(states, {
-  Idle: {
-    execute: (req: SomeRequest, somethingElse: boolean) => {
-      console.log('execute', req.id, somethingElse)
-
-      return states.Pending(req)    
-    }
+// ---cut---
+const machine = createMachine({
+  states: defineStates({
+    Idle: {},
+    Pending: (url: string ) => ({ url }),
+    Rejected: (error: Error) => ({ error }),
+    Resolved: ({ someResult }: { someResult: string }) => ({ someResult }),
+  }),
+  initialState: 'Idle',
+  transitions: {
+    Idle: {
+      execute: 'Pending'
+    },
+    Pending: {
+      resolve: 'Resolved',
+      reject: 'Rejected'
+    },
+    Rejected: {},
+    Resolved: {}
   },
-  Pending: {
-    resolve: 'Resolved',
-    reject: 'Rejected'
-  },
-  Rejected: {},
-  Resolved: {}
 })
+machine.send('execute', 'https://mysite')
+const { url } = machine.getState().as('Pending').data
+machine.send('reject', new Error('error'))
+const { error } = machine.getState().as('Rejected').data
 
-const initialState = states.Idle()
-const sampleRequestState = states.Pending({ id: 123 })
-const sampleResponse = states.Resolved({ someResult: 'ok' })
-const sampleError = states.Rejected(new Error('nope'))
-
-const machine = withEvents(Machine.create(initialState))
-machine.event.execute({ id: 123 }, true)
-machine.event.reject(new Error('error'))
-
+machine.getState().match({
+  Idle() { console.log('Idle') },
+  Pending({ url }) { console.log('Pending', url) },
+  Rejected({ error }) { console.log('Rejected', error)},
+  Resolved({ someResult }) { console.log('Resolved', someResult)}
+})
