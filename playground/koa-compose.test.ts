6cb402e47f0de40ad22d50a529f69c0b6343ab2f
Winston Fassett <winston.fassett@gmail.com>
2023-11-12 00:38:20 -0600

    ok

--- playground/koa-compose.test.ts
+++ playground/koa-compose.test.ts
@@ -1,36 +1,40 @@
 import { describe, expect, it, vi } from "vitest";
-import { applyMethodware } from "../playground/koa-compose";
+import { applyMethodware } from "../src/dev/middleware4";
 
 describe("applyMethodware", () => {
   it("applies middleware to sync target method", async () => {
-    const middleware1 = vi.fn((next) => (value: number) => next(value + 1));
-    const middleware2 = vi.fn((next) => (value: number) => next(value * 2));
+    const mw1 = (next: any) => (value: number) => next(value + 1)
+    const mw2 = (next: any) => (value: number) => next(value * 2)
+    const middleware1 = vi.fn(mw1);
+    const middleware2 = vi.fn(mw2);
     const subject = {
       addOne: (value: number) => value + 1,
     };
+    const orig = subject.addOne;
     applyMethodware(subject, "addOne", middleware1, middleware2);
     const result = await subject.addOne(1);
     expect(result).toBe(4);
-    expect(middleware1).toHaveBeenCalledWith(subject.addOne);
-    expect(middleware2).toHaveBeenCalledWith(middleware1);
+    expect(middleware1).toHaveBeenCalledWith(orig);
+    expect(middleware2).toHaveBeenCalled()
   });
 
-  // it("applies middleware to async target method", async () => {
-  //   const middleware1 = vi.fn((next) => async (value: number) =>
-  //     next(value + 1)
-  //   );
-  //   const middleware2 = vi.fn((next) => async (value: number) =>
-  //     next(value * 2)
-  //   );
-  //   const subject = {
-  //     async addOneAsync(value: number) {
-  //       return value + 1;
-  //     },
-  //   };
-  //   applyMethodware(subject, "addOneAsync", middleware1, middleware2);
-  //   const result = await subject.addOneAsync(1);
-  //   expect(result).toBe(4);
-  //   expect(middleware1).toHaveBeenCalledWith(subject.addOneAsync);
-  //   expect(middleware2).toHaveBeenCalledWith(middleware1);
-  // });
+  it("applies middleware to async target method", async () => {
+    const middleware1 = vi.fn((next) => async (value: number) =>
+      next(value + 1)
+    );
+    const middleware2 = vi.fn((next) => async (value: number) =>
+      next(value * 2)
+    );
+    const subject = {
+      async addOneAsync(value: number) {
+        return value + 1;
+      },
+    };
+    const orig = subject.addOneAsync
+    applyMethodware(subject, "addOneAsync", middleware1, middleware2);
+    const result = await subject.addOneAsync(1);
+    expect(result).toBe(4);
+    expect(middleware1).toHaveBeenCalledWith(orig);
+    expect(middleware2).toHaveBeenCalled();
+  });
 });
--- src/dev/middleware4.ts
+++ src/dev/middleware4.ts
@@ -1,11 +1,6 @@
-// import { Func } from "../types";
-
 type Func<T extends any[], R> = (...args: T) => R;
 
 export type Middleware<T extends any[], R> = (fn: Func<T, R>) => Func<T, R>;
-
-// Define a Middleware type
-// export type Middleware<T extends any[], R> = (fn: (...args: T) => R) => (...args: T) => R;
 
 export function applyMiddleware<T extends any[], R, M extends R>(
   targetFunction: Func<T, R>,
@@ -22,10 +17,20 @@ export function applyMiddleware<T extends any[], R,  M extends R>(
   ...middlewares: Middleware<T, R>[]
 ): Func<T, R> | Promise<Func<T, R>> {
   let currentFn: Func<T, R> | Promise<Func<T, R>> = targetFunction;
-
   for (const middleware of middlewares) {
     currentFn = middleware(currentFn);
   }
-
   return currentFn;
+}
+
+export function applyMethodware<
+  T extends Record<K, (...args: any[]) => any>,
+  K extends keyof T,
+>(subject: T, key: K, ...middlewares: Middleware<Parameters<T[K]>, ReturnType<T[K]>>[]) {
+  const inner = subject[key];  
+  const composed = (applyMiddleware as any)(inner, ...middlewares)
+  subject[key] = composed;
+  return () => {
+    subject[key] = inner;
+  };
 }
