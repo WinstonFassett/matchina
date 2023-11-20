13bf36eb7d197a49449617882663fd9d3713ee83
Winston Fassett <winston.fassett@gmail.com>
2023-11-20 07:04:16 -0600

    idea

---
+++ src/extras/middleware/sync-middleware.ts
@@ -0,0 +1,26 @@
+
+type SyncMiddleware<T> = (value: T) => T;
+
+function pipeValue<T>(middlewares: SyncMiddleware<T>[], value: T): T {
+    for (const middleware of middlewares) {
+        value = middleware(value);
+    }
+    return value;
+}
+
+type SyncOptionalReturnMiddleware<T> = (value?: T) => T | undefined;
+
+function runSyncOptionalMiddlewares<T>(middlewares: SyncOptionalReturnMiddleware<T>[], value?: T): T | undefined {
+    for (const middleware of middlewares) {
+        value = middleware(value) ?? value;
+    }
+    return value;
+}
+
+function runSyncStoppableMiddlewares<T>(middlewares: SyncOptionalReturnMiddleware<T>[], value?: T): T | undefined {
+  for (const middleware of middlewares) {
+      if (value === undefined) return value;
+      value = middleware(value)
+  }
+  return value;
+}
