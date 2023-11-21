5fa647f5c5550e7d5d38812920aaca649f12825d
Winston Fassett <winston.fassett@gmail.com>
2023-11-21 15:18:32 -0600

    better

--- src/dev/v3/usage.test.ts
+++ src/dev/v3/usage.test.ts
@@ -4,18 +4,11 @@ import { after, before, guard, machineSetup, setupMachine } from './machine-setu
 import { createStateMachine } from './state-machine';
 import { expect, describe, it } from 'vitest';
 
-// Define common states
-const states = defineStates({
-  Idle: undefined,
-  Pending: (x: number) => ({ s: `#${x}` }),
-  Resolved: (ok: boolean) => ({ ok }),
-  Rejected: (err: Error) => ({ err })
-});
 
 describe('setupMachine', () => {
-  const create = () =>
-  {
-    const m = createStateMachine({
+
+  it('should transition correctly', () => {
+    const machine = createStateMachine({
       Idle: {
         'start': { key: 'Running' }
       },
@@ -24,79 +17,87 @@ describe('setupMachine', () => {
       }
     }, { key: 'Idle', data: undefined });
   
-    setupMachine(m)(
+    setupMachine(machine)(
       guard(ev => true),
       before(ev => console.log('before', ev))
     );
-    return m
-  } 
-
-  it('should transition', () => {
-    const m1 = create()
-    expect(m1.getState().key).toBe('Idle');    
-    m1.send('start');
-    expect(m1.getState().key).toBe('Running');
-    m1.send('stop');
-    expect(m1.getState().key).toBe('Idle');
+    expect(machine.getState().key).toBe('Idle');    
+    machine.send('start');
+    expect(machine.getState().key).toBe('Running');
+    machine.send('stop');
+    expect(machine.getState().key).toBe('Idle');
   });
 });
 
 describe('machineSetup', () => {
-  const m2 = createStateMachine({
-    Idle: {
-      'start': { key: 'Running' }
-    },
-    Running: {
-      'stop': { key: 'Idle'}
-    }
-  }, { key: 'Idle', data: undefined } as { key: 'Idle' | 'Pending' | 'Done'; data: undefined; });
-
-  machineSetup<typeof m2>(
-    guard(ev => true),
-    before(ev => console.log('before', ev))
-  )(m2);
-
-  it('should transition from Idle to Running when started', () => {
-    m2.send('start');
-    expect(m2.getState().key).toBe('Running');
+  
+  it('should transition correctly', () => {
+    const machine = createStateMachine({
+      Idle: {
+        'start': { key: 'Running' }
+      },
+      Running: {
+        'stop': { key: 'Idle'}
+      }
+    }, { key: 'Idle', data: undefined } as { key: 'Idle' | 'Pending' | 'Done'; data: undefined; });
+  
+    machineSetup<typeof machine>(
+      guard(ev => true),
+      before(ev => console.log('before', ev))
+    )(machine);
+    machine.send('start');
+    expect(machine.getState().key).toBe('Running');
+    machine.send('stop');
+    expect(machine.getState().key).toBe('Idle');
   });
 
-  it('should transition from Running to Idle when stopped', () => {
-    m2.send('start');
-    m2.send('stop');
-    expect(m2.getState().key).toBe('Idle');
-  });
+  
 });
 
 describe('factory-machine', () => {
-  const m4 = createFactoryMachine(states, {
-    Idle: { execute: 'Pending' },
-    Pending: { resolve: 'Resolved', reject: 'Rejected' },
-    Resolved: {},
-    Rejected: {}
-  }, states.Idle());
+  const create = () => {
+    const states = defineStates({
+      Idle: undefined,
+      Pending: (x: number) => ({ s: `#${x}` }),
+      Resolved: (ok: boolean) => ({ ok }),
+      Rejected: (err: Error) => ({ err })
+    });
+  
+    const machine = createFactoryMachine(states, {
+      Idle: { execute: 'Pending' },
+      Pending: { resolve: 'Resolved', reject: 'Rejected' },
+      Resolved: {},
+      Rejected: {}
+    }, states.Idle());
 
-  setupMachine(m4)(
-    guard(ev => ev.type !== 'execute' || ev.params[0] > 0),
-    before(ev => { if (ev.type == 'execute') { console.log('executing') } }),
-    after(ev => console.log(ev.to.match<any>({
-      Pending: (ev) => ev.s,
-      Resolved: (ev) => ev.ok,
-      Rejected: (ev) => ev.err,
-      _: () => false
-    }))),
-  );
-
+    setupMachine(machine)(
+      guard(ev => ev.type !== 'execute' || ev.params[0] > 0),
+      before(ev => { if (ev.type == 'execute') { console.log('executing') } }),
+      after(ev => console.log(ev.to.match<any>({
+        Pending: (ev) => ev.s,
+        Resolved: (ev) => ev.ok,
+        Rejected: (ev) => ev.err,
+        _: () => false
+      }))),
+    );
+    return machine
+  }
   it('should transition through states when executed', () => {
-    m4.send('execute', 1);
-    expect(m4.getState().key).toBe('Pending');
+    let machine = create()
+    machine.send('execute', 1);
+    expect(machine.getState().key).toBe('Pending');
     
-    // Depending on your logic, you may want to further transition and test
-    // For example, transition to 'Resolved' or 'Rejected' based on some condition
-    // m4.send('resolve', true);
-    // or
-    // m4.send('reject', new Error('Some error'));
+    machine.send('resolve', true);
+    expect(machine.getState().key).toBe('Resolved');
+    
+    expect(machine.getState().as('Resolved').data.ok).toBe(true);
+    
+    machine = create()
+    machine.send('execute', 2)
 
-    // Add assertions here to check the expected state transitions
+    machine.send('reject', new Error('Some error'));
+    expect(machine.getState().key).toBe('Rejected');
+    expect(machine.getState().as('Rejected').data.err).toBeInstanceOf(Error);
+    expect(machine.getState().as('Rejected').data.err.message).toBe('Some error');
   });
 });
