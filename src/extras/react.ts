052ebf83c0f5655c42cdedba281cfd361eb09607
Winston Fassett <winston.fassett@gmail.com>
2023-11-25 06:15:42 -0600

    react note

--- src/extras/react.ts
+++ src/extras/react.ts
@@ -1,6 +1,11 @@
 import React, { useCallback } from "react";
 import { Listen } from "./nanosubscriber";
 
+// prefer a lightweight subscriber for this machine
+// just use notify or something
+// may be as easy as const unsub = notify(listener)(machine);
+// or setupMachine(machine)(notify(listener));
+
 export function useMachine<Change>(machine: {
   subscribe: (listener: Listen<Change>) => () => void;
   getChange: () => Change;
