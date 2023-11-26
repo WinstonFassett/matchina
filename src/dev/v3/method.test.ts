8fd8eca0826558aa2248461a4d6655b9e1809734
Winston Fassett <winston.fassett@gmail.com>
2023-11-26 07:12:48 -0600

    tests and fixes for method

---
+++ src/dev/v3/method.test.ts
@@ -0,0 +1,111 @@
+import { describe, it, expect, vi } from "vitest";
+import { condition, methodExtend, methodListenTo, methodUse } from "./method";
+
+describe("methodExtend", () => {
+  it("should extend the method correctly", () => {
+    const obj = {
+      method: (value: string) => value.toUpperCase(),
+    };
+
+    methodExtend(obj, "method", (inner) => {
+      return (value: string) => {
+        const result = inner(value);
+        return result + "!";
+      };
+    });
+
+    expect(obj.method("hello")).toBe("HELLO!");
+  });
+
+  it("should restore the original method", () => {
+    const obj = {
+      method: (value: string) => value.toUpperCase(),
+    };
+
+    const restore = methodExtend(obj, "method", (inner) => {
+      return (value: string) => {
+        const result = inner(value);
+        return result + "!";
+      };
+    });
+
+    restore();
+
+    expect(obj.method("hello")).toBe("HELLO");
+  });
+});
+
+describe("methodUse", () => {
+  it("should extend the method correctly", () => {
+    const obj = {
+      method: (value: string) => value.toUpperCase(),
+    };
+    
+    const use = methodUse("method");
+
+    use((inner) => {
+      return (value: string) => {
+        const result = inner(value);
+        return result + "!";
+      };
+    })(obj);
+
+    expect(obj.method("hello")).toBe("HELLO!");
+  });
+
+  it("should restore the original method", () => {
+    const obj = {
+      method: (value: string) => value.toUpperCase(),
+    };
+
+    const use = methodUse("method");
+
+    const restore = use((inner) => {
+      return (value: string) => {
+        const result = inner(value);
+        return result + "!";
+      };
+    })(obj);
+
+    restore();
+
+    expect(obj.method("hello")).toBe("HELLO");
+  });
+});
+
+describe('methodListenTo', () => {
+  it('should listen to the method and call the provided function', () => {
+    const obj = { method: (value: string) => value.toUpperCase() };
+    const mockFn = vi.fn((value: string) => `Hello, ${value}`);
+
+    methodListenTo('method')(mockFn)(obj);
+    const result = obj.method('world');
+
+    expect(mockFn).toHaveBeenCalled();
+    expect(result).toBe('WORLD');
+  });
+});
+
+describe('condition', () => {
+  it('should call the entryListener when the test passes', () => {
+    const mockTest = vi.fn((value: number) => value > 0);
+    const mockEntryListener = vi.fn((value: number) => {});
+
+    const conditionFn = condition(mockTest, mockEntryListener);
+    conditionFn(1);
+
+    expect(mockTest).toHaveBeenCalled();
+    expect(mockEntryListener).toHaveBeenCalled();
+  });
+
+  it('should not call the entryListener when the test fails', () => {
+    const mockTest = vi.fn((value: number) => value > 0);
+    const mockEntryListener = vi.fn((value: number) => {});
+
+    const conditionFn = condition(mockTest, mockEntryListener);
+    conditionFn(-1);
+
+    expect(mockTest).toHaveBeenCalled();
+    expect(mockEntryListener).not.toHaveBeenCalled();
+  });
+});
--- src/dev/v3/method.ts
+++ src/dev/v3/method.ts
@@ -2,22 +2,31 @@ export type HasMethod<K extends string> = {
   [key in K]: (...args: any[]) => any;
 };
 
-export function methodExtend<
-  T,  
-  K extends keyof T,
-  M extends T[K] extends (...args: any[]) => any ? T[K] : never  
->(target: T, methodName: K, extend: (inner: M) => M) {
-  const original = target[methodName] as M;
+type FunctionType<T, K extends keyof T> = T[K] extends (...args: any[]) => any
+  ? T[K]
+  : never;
+
+export function methodExtend<T, K extends keyof T>(
+  target: T,
+  methodName: K,
+  extend: (inner: FunctionType<T, K>) => FunctionType<T, K>
+) {
+  const original = target[methodName] as FunctionType<T, K>;
   target[methodName] = extend(original.bind(target));
   return () => {
     target[methodName] = original;
   };
 }
+
 export const methodUse =
   <K extends string>(methodName: K) =>
-  <T extends HasMethod<K>>(fn: (inner: T[K]) => T[K]) =>
-  (target: T) =>
-    methodExtend(target, methodName, fn(target[methodName]));
+  <T extends HasMethod<K>>(
+    fn: (inner: FunctionType<T, K>) => FunctionType<T, K>
+  ) =>
+  (target: T) => {
+    return methodExtend(target, methodName, fn);
+  } 
+
 
 export type ExitListener<P extends any[]> = (...params: P) => void;
 export type EntryListener<P extends any[]> = (...params: P) => void | ExitListener<P>;
@@ -27,8 +36,8 @@ export const methodListenTo =
   <K extends string>(methodName: K) =>
   <T extends HasMethod<K>>(fn: T[K]) =>
   (target: T) => {
-    return methodUse(methodName)((...params) => {
-      const res = target[methodName](...params);
+    return methodUse(methodName)(inner => (...params) => {
+      const res = inner(...params);
       fn(...params);
       return res;
     })(target);
