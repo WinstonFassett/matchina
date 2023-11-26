815eb083b59cc717a7d21dd971b9f40db14066dc
Winston Fassett <winston.fassett@gmail.com>
2023-11-25 19:06:18 -0600

    remove when

--- src/dev/v3/machine-setup.ts
+++ src/dev/v3/machine-setup.ts
@@ -29,39 +29,27 @@ export const after = methodListenTo("after");
 export const notify = methodListenTo("notify");
 //#endregion
 
-// export const methodUse =
-//   <K extends string>(methodName: K) =>
-//   <T extends HasMethod<K>>(fn: (inner: T[K]) => T[K]) =>
-//   (target: T) =>
-//     methodExtend(methodName, target, fn(target[methodName]));
-
-function whenEventSetup () {
-  return function forMachine(machine) {
-    // map over
-  }
-}
-
-export const when =
-  <
-    E extends ChangeCommandEvent,
-  >(    
-    test: (ev: E) => boolean,
-    enterListener: (ev: E) => void | ((ev: E) => void),
-  ) => (target: HasMethod<'before'> & HasMethod<'after'>) =>  {
-    let exitListener: void | ((ev: E) => void);
-    const unbefore = before((ev) => {
-      if (test(ev)) return; // not an exit(?)
-      exitListener?.(ev);
-      exitListener = undefined;
-    })(target);
-    const unafter = after((ev) => {
-      if (test(ev)) exitListener = enterListener(ev);
-    })(target);
-    return () => {
-      unbefore();
-      unafter();
-    };
-  };
+// export const when =
+//   <
+//     E extends ChangeCommandEvent,
+//   >(    
+//     test: (ev: E) => boolean,
+//     enterListener: (ev: E) => void | ((ev: E) => void),
+//   ) => (target: HasMethod<'before'> & HasMethod<'after'>) =>  {
+//     let exitListener: void | ((ev: E) => void);
+//     const unbefore = before((ev) => {
+//       if (test(ev)) return; // not an exit(?)
+//       exitListener?.(ev);
+//       exitListener = undefined;
+//     })(target);
+//     const unafter = after((ev) => {
+//       if (test(ev)) exitListener = enterListener(ev);
+//     })(target);
+//     return () => {
+//       unbefore();
+//       unafter();
+//     };
+//   };
 
 // These are pretty generic, could be named so
 export function machineSetup<M>(...extenders: ((machine: M) => () => void)[]) {
