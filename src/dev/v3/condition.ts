c80d0a1a586724126c58bff50dfd7a911fe38989
Winston Fassett <winston.fassett@gmail.com>
2023-11-29 06:50:28 -0600

    cp

--- src/dev/v3/condition.ts
+++ src/dev/v3/condition.ts
@@ -1,9 +1,10 @@
-import { EntryListener } from "./method";
+export type ExitListener<E> = (event: E) => void;
+export type EntryListener<E> = (event: E) => (void | ExitListener<E>);
 
 
 export function condition<E>(
   test: (ev: E) => boolean,
-  entryListener: EntryListener<[E]>
+  entryListener: EntryListener<E>
 ) {
   let exitListener: void | ((ev: E) => void);
   return (ev: E) => {
