f3f87c442c61d89932cc7e3203c0b5255629f40a
Winston Fassett <winston.fassett@gmail.com>
2023-12-03 22:25:27 -0600

    84b nanosubscriber

--- src/extras/nanosubscriber.ts
+++ src/extras/nanosubscriber.ts
@@ -5,18 +5,18 @@ export type Emit<T> = (value: T) => void;
 
 export const nanosubscriber = <T>(
   listeners = [] as Listen<T>[]
-): [Subscribe<T>, Emit<T>] =>
+): [Subscribe<T>, Emit<T>, Listen<T>[]] =>
   [
     (listener: Listen<T>) => {
       listeners.push(listener);
       return () => {
         listeners = listeners.filter((l) => l !== listener);
-        // listeners.splice(listeners.indexOf(listener) >>> 0, 1)
       };
     },
     (value: T) => {
       for (const listener of listeners) {
         listener(value);
       }
-    }
+    },
+    listeners,
   ]
