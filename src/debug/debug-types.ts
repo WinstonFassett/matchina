48f138427b4ee05f792774208f9f3495698f4e79
Winston Fassett <winston.fassett@gmail.com>
2023-12-09 08:11:37 -0600

    cp

--- src/debug/debug-types.ts
+++ src/debug/debug-types.ts
@@ -1,6 +1,8 @@
-// primitive theoretical types. do not use lol
+import { Funcware, AbortableEventHandler, abortableEventware, functionTap, HasMethod, MethodOf, methodExtender } from "../ext";
+import { FlatMemberUnionToIntersection, Func, Simplify } from "../utility-types";
+import { Effect, Middleware } from "../types";
 
-import { FlatMemberUnionToIntersection, Simplify } from "../utility-types";
+// primitive theoretical types. do not use lol
 
 // interface Change<T> {
 //   from: T;
@@ -198,11 +200,7 @@ export function createFactoryMachine<
 >(
   states: FC['states'],
   transitions: FC['transitions'],
-  // initialState: AnyFactoryState<SF>,
   init: KeysWithZeroArgs<FC['states']> | AnyFactoryState<FC['states']>,
-  // FunctionWithParameters<T> extends true
-  //     ? { key: string }
-  //     : keyof T | undefined
 ): AltFactoryMachine<FC> {
   const initialState = (
     typeof init === "string" ? states[init]({}) : init
@@ -302,8 +300,8 @@ const slowlyAddTwoNumbers = (
 
 
 const m : AltFactoryMachine<{
-  states: {},
-  transitions: {}
+  states: PromiseStates<typeof slowlyAddTwoNumbers>,
+  transitions: typeof PromiseTransitions
 }> = {} as any;
 
 function machineRelatedFunction(m: AltFactoryMachine<any>) {
@@ -312,7 +310,8 @@ function machineRelatedFunction(m: AltFactoryMachine<any>) {
 
 machineRelatedFunction(m)
 
-
+const mapi = withApi(m);
+mapi.api.reject(new Error('nope'))
 
 export function createApi<
  M extends AltFactoryMachine<any>
@@ -415,4 +414,217 @@ export type StateEventTransitionFunc<
         key: Transitions[TransitionStateKey][EventKey];
       }
     : never;
+};
+
+
+// HOOKS:
+
+
+//#region Adapters
+export type Adapters<E extends StateMachineEvent = StateMachineEvent> = {
+  [key: string]: Func;
+} & {
+  transition: (middleware: Middleware<E>) => Funcware<StateMachinery<E>["transition"]>;
+  update: (middleware: Middleware<E>) => Funcware<StateMachinery<E>["update"]>;
+  resolve: <F extends StateMachinery<E>["resolve"]>(resolveFn: F) => Funcware<F>;
+  guard: (guardFn: StateMachinery<E>['guard']) => Funcware<StateMachinery<E>['guard']>;
+  handle: (handleFn: StateMachinery<E>['handle']) => Funcware<StateMachinery<E>['handle']>;
+  before: (abortware: AbortableEventHandler<E>) => Funcware<Transform<E>>;
+  leave: Transform<Effect<E>, Funcware<Effect<E>>>;
+  after: Transform<Effect<E>, Funcware<Effect<E>>>;
+  enter: Transform<Effect<E>, Funcware<Effect<E>>>;
+  effect: Transform<Effect<E>, Funcware<Effect<E>>>;
+  notify: Transform<Effect<E>, Funcware<Effect<E>>>;
+};
+type Transform<I, O = I> = (source: I) => O;
+
+export const HookAdapters = {
+  transition: (middleware) => (next) => (ev) => { middleware(ev, next); },
+  update: (middleware) => (next) => (ev) => { middleware(ev, next); },
+  resolve: (resolveFn) => (next) => (ev) => resolveFn(ev) ?? next(ev),
+  guard: (guardFn) => (inner) => combineGuards(inner, guardFn),
+  handle: (handleFn) => (inner) => composeHandlers(handleFn, inner),
+  before: (abortware) => abortableEventware(abortware),
+  leave: functionTap,
+  after: functionTap,
+  enter: functionTap,
+  effect: functionTap,
+  notify: functionTap,
+} as Adapters;
+//#endregion
+
+// #region Interceptors
+// export const send = methodHook("send");
+export const before = hookSetup("before");
+export const transition = hookSetup("transition");
+export const resolve = hookSetup("resolve");
+export const guard = hookSetup('guard')
+export const update = hookSetup("update");
+export const handle = hookSetup("handle");
+//#endregion
+
+// #region Effects
+export const effect = hookSetup("effect");
+export const leave = hookSetup('leave')
+export const enter = hookSetup("enter");
+export const after = hookSetup("after");
+export const notify = hookSetup("notify");
+//#endregion
+
+export const onBefore = machineHook("before");
+export const onTransition = machineHook("transition");
+export const onResolve = machineHook("resolve");
+export const onGuard = machineHook("guard");
+export const onUpdate = machineHook("update");
+export const onHandle = machineHook("handle");
+export const onEffect = machineHook("effect");
+export const onLeave = machineHook("leave");
+export const onEnter = machineHook("enter");
+export const onAfter = machineHook("after");
+export const onNotify = machineHook("notify");
+
+
+
+function machineHook<K extends string & keyof Adapters>(key: K) {
+  return <T extends HasMethod<K>>(
+    machine: T,
+    fn: MethodOf<T,K>
+  ) => methodExtender<K>(key)
+    (HookAdapters[key](fn))(machine)  
+}
+
+function hookSetup<K extends string & keyof Adapters>(key: K) {
+  return <T extends HasMethod<K>>(
+    ...config: Parameters<Adapters<Parameters<MethodOf<T, K>>[0]>[K]>
+  ) =>
+    methodExtender<K>(key)(HookAdapters[key](...config)) as (
+      target: T,
+    ) => () => void;
+}
+
+function composeHandlers<E extends StateMachineEvent>(
+  outer: (value: E) => E | undefined,
+  inner: (value: E) => E | undefined,
+): (value: E) => E | undefined {
+  return (ev) => outer(inner(ev) as any);
+}
+
+function combineGuards<E extends StateMachineEvent>(
+  first: (value: E) => boolean,
+  next: (value: E) => boolean,
+): (value: E) => boolean {
+  return (ev) => {
+    const res = first(ev) && next(ev);
+    return res;
+  };
+}
+
+
+// TYPEGUARDS
+
+export type AnyKeyedChangeEvent = {
+  type: string
+  to: { key: string }
+  from: { key: string }  
+}
+
+export type KeyedChangeEvent<Type extends string, ToKey extends string, FromKey extends string> = {
+  type: Type
+  to: { key: ToKey }
+  from: { key: FromKey }  
+}
+
+export function hasKeyValue<T, K extends PropertyKey, V>(
+  obj: T,
+  key: K,
+  values: V | V[],
+): obj is T & Record<K, V> {
+  if (!Array.isArray(values)) {
+    return (obj as Record<K, V>)[key] === values;
+  }
+  return values.includes((obj as Record<K, V>)[key]);
+}
+
+function matchKey<T>(keyOrKeys: T | T[] | undefined, value: T) {
+  if (keyOrKeys === undefined) {
+    return true;
+  }
+  return Array.isArray(keyOrKeys)
+    ? keyOrKeys.includes(value)
+    : keyOrKeys === value;
+}
+
+export type KeyedChangeEventFilter<E> = Filters<AnyKeyedChangeEvent>
+
+export function isKeyedChangeEvent<
+  E
+>(
+  filter: KeyedChangeEventFilter<E>,
+  event: E,
+): event is E & AnyKeyedChangeEvent {
+  const subject = event as any;
+  const matched =
+    matchKey(filter.to, subject?.to?.key) &&
+    matchKey(filter.type, subject?.type) &&
+    matchKey(filter.from, subject?.from?.key);
+  // console.log('match?', matched, filter, event.type)
+  return matched;
+}
+export function isChangeTypeToFrom<
+  E,
+  Type extends string,
+  ToKey extends string,
+  FromKey extends string,
+>(
+  event: E,
+  type?: Type | Type[],
+  to?: ToKey | ToKey[],
+  from?: FromKey | FromKey[],
+): event is E & KeyedChangeEvent<Type, FromKey, ToKey> {
+  const subject = event as any;
+  return (
+    matchKey(to, subject?.to?.key) &&
+    matchKey(type, subject?.type) &&
+    matchKey(from, subject?.from?.key)
+  );
+}
+export function asChangeTypeToFrom<
+  E,
+  Type extends string,
+  ToKey extends string,
+  FromKey extends string,
+>(
+  event: E,
+  type?: Type | Type[],
+  to?: ToKey | ToKey[],
+  from?: FromKey | FromKey[],
+): E & KeyedChangeEvent<Type, FromKey, ToKey> {
+  if (isChangeTypeToFrom(event, type, to, from)) {
+    return event;
+  }
+  throw new Error("not a match");
+}
+
+
+type Filters<T> = object & {
+  [K in keyof T]?: T[K] | T[K][];
+};
+
+type HookConfig<T> = {
+  [K in keyof T]?: T[K] | T[K][];
+};
+
+export type TransitionHookExtensions<E extends StateMachineEvent> = {
+  begin: AbortableEventHandler<E>;
+  resolve: Funcware<(ev: Partial<E>) => E>;
+  transition: AbortableEventHandler<E>;
+  guard: StateMachinery<E>['guard'];
+  handle: StateMachinery<E>['handle'];
+  before: AbortableEventHandler<E>;
+  effect: Effect<E>;
+  leave: Effect<E>;
+  enter: Effect<E>;
+  notify: Effect<E>;
+  after: Effect<E>;
+  end: Effect<E>;
 };
