ed9bfa255a4a4ae89c2d41dc68bfd8a6ce223565
Winston Fassett <winston.fassett@gmail.com>
2024-06-30 11:09:41 -0500

    factory machine lifecycle

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ export * from "./extras/with-nanosubscribe";
 export * from "./extras/with-reset";
 export * from "./factory-machine";
 export * from "./factory-machine-event-api";
+export * from "./factory-machine-lifecycle";
 export * from "./matchbox";
 export * from "./states";
 export * from "./promise-handle";
