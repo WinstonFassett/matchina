c118b3561c657e5fcf017535b5ee960772c6afde
Winston Fassett <winston.fassett@gmail.com>
2024-06-30 10:50:22 -0500

    promise handle, machine

--- src/index.ts
+++ src/index.ts
@@ -6,5 +6,7 @@ export * from "./factory-machine";
 export * from "./factory-machine-event-api";
 export * from "./matchbox";
 export * from "./states";
+export * from "./promise-handle";
+export * from "./promise-machine";
 export * from "./state-machine";
 export * from "./state-machine-pure";
