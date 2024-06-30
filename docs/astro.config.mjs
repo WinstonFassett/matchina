426da4680d2effb6007c914be509e613fefeb8bb
Winston Fassett <winston.fassett@gmail.com>
2024-06-30 18:28:25 -0500

    use page/index.html

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -55,31 +55,31 @@ export default defineConfig({
           items: [
             {
               label: "Quickstart",
-              link: "/matchina/guides/quickstart/",
+              link: "/matchina/guides/quickstart/index.html",
             },
             {
               label: "Features",
-              link: "/matchina/guides/inside/",
+              link: "/matchina/guides/inside/index.html",
             },
             {
               label: "State Machines",
-              link: "/matchina/guides/machines/",
+              link: "/matchina/guides/machines/index.html",
             },
             {
               label: "Hooks and Lifecycle",
-              link: "/matchina/guides/hooks/",
+              link: "/matchina/guides/hooks/index.html",
             },
             {
               label: "Type Guards",
-              link: "/matchina/guides/typeguards/",
+              link: "/matchina/guides/typeguards/index.html",
             },
             {
               label: "Extras",
-              link: "/matchina/guides/extras/",
+              link: "/matchina/guides/extras/index.html",
             },
             {
               label: "Integrations",
-              link: "/matchina/guides/integrations/",
+              link: "/matchina/guides/integrations/index.html",
             },
           ],
         },
@@ -88,30 +88,30 @@ export default defineConfig({
           items: [
             {
               label: "Promise - simple", 
-              link: "/matchina/examples/fetch-simple/"
+              link: "/matchina/examples/fetch-simple/index.html"
             },
             { 
               label: 'Stopwatch (useEffect)',
-              link: '/matchina/examples/stopwatch-with-react-state-and-effects/'
+              link: '/matchina/examples/stopwatch-with-react-state-and-effects/index.html'
             },
             { label: 'Stopwatch (onLifecycle setup)',
-              link: '/matchina/examples/stopwatch-with-react-state-using-lifecyle-instead-of-useeffect'
+              link: '/matchina/examples/stopwatch-with-react-state-using-lifecyle-instead-of-useeffect/index.html'
             },
             { 
               label: 'Stopwatch (data and setup)',
-              link: '/matchina/examples/stopwatch-using-data-and-hooks/'
+              link: '/matchina/examples/stopwatch-using-data-and-hooks/index.html'
             },            
             { 
               label: 'Stopwatch (functional with data)',
-              link: '/matchina/examples/stopwatch-using-data-and-transition-functions/'
+              link: '/matchina/examples/stopwatch-using-data-and-transition-functions/index.html'
             },
             { 
               label: 'Stopwatch (machine effect hooks)',
-              link: '/matchina/examples/stopwatch-with-react-state-and-state-effects/'
+              link: '/matchina/examples/stopwatch-with-react-state-and-state-effects/index.html'
             },
             {
               label: "Fetch - advanced", 
-              link: "/matchina/examples/fetch-plus/"
+              link: "/matchina/examples/fetch-plus/index.html"
             }
           ]
         },
@@ -120,19 +120,19 @@ export default defineConfig({
           items: [
             {
               label: "Effects",
-              link: "/matchina/guides/effects/",
+              link: "/matchina/guides/effects/index.html",
             },
             {
               label: "Context", 
-              link: "/matchina/guides/context/"
+              link: "/matchina/guides/context/index.html"
             },
             {
               label: "Union Machines",
-              link: "/matchina/guides/union-machines/",
+              link: "/matchina/guides/union-machines/index.html",
             },
             {
               label: "Timsy Compatibility",
-              link: "/matchina/guides/timsy/",
+              link: "/matchina/guides/timsy/index.html",
             },
           ],
           
