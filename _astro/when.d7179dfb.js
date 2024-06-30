c3f6c2337d66332f31473b1a0e579f80fe5149af
Winston Fassett <winston.fassett@gmail.com>
2024-06-30 16:47:34 -0500

    publish docs to gh-pages

---
+++ _astro/when.d7179dfb.js
@@ -0,0 +1,1 @@
+function r(e,n){let i;return t=>{i&&(i(t),i=void 0),e(t)&&(i=n(t))}}export{r as w};
---
+++ _astro/zen.ec9d499f.js
@@ -0,0 +1,1 @@
+import{c as e,s as r}from"./states.c77b78b3.js";function g(t){return{...e(t),get state(){return t.getState()},get change(){return t.getChange()},get machine(){return t},setup:r(t)}}export{g as z};
