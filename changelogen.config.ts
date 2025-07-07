a93551812d1479d012959825734888d1802bf3e3
Winston Fassett <winston.fassett@gmail.com>
2025-07-07 16:36:10 -0500

    changelogen

---
+++ changelogen.config.ts
@@ -0,0 +1,9 @@
+import { defineConfig } from 'changelogen';
+
+export default defineConfig({
+  github: 'WinstonFassett/matchina',
+  types: true,
+  output: 'CHANGELOG.md',
+  release: true,
+  scopeMap: {},
+});
