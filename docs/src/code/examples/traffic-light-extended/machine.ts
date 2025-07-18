b92b2c6808b801573dd16d6b38e39370a3ad29c5
Winston Fassett <winston.fassett@gmail.com>
2025-07-18 13:54:38 -0600

    feckcleanup

--- docs/src/code/examples/traffic-light-extended/machine.ts
+++ docs/src/code/examples/traffic-light-extended/machine.ts
@@ -1,26 +1,4 @@
-import { change, effect, nanosubscriber } from "matchina";
-import { createMachine, createTransitionMachine, defineStates, enter, onLifecycle, setup, type StateMachineEvent, whenState, withApi } from "matchina";
-
-  interface State {
-    crossingRequested?: boolean;
-  }
-
-function atom<T>(initialValue: T) {
-  let value = initialValue;
-  const [subscribe, emit] = nanosubscriber<T>();
-  return {
-    get: () => value,
-    set: (newValue: T) => { 
-      value = newValue; 
-      emit(value);
-    },
-    update: (changes: Partial<T>) => {
-      value = { ...value, ...changes };
-      emit(value);
-    },
-    subscribe,
-  };
-}
+import { createMachine, defineStates, enter, setup, whenState, withApi } from "matchina";
 
 export const createExtendedTrafficLightMachine = () => {
   const pedestrianStates = defineStates({
@@ -29,26 +7,16 @@ export const createExtendedTrafficLightMachine = () => {
     Error: undefined
   })
 
-  // const data = atom({
-  //   // ok: true,
-  //   crossingRequested: false,
-  // })
   interface State {
     key: string,
     crossingRequested?: boolean;
     walkWarningDuration?: number;
   }
 
-  // const sharedState = createTransitionMachine<StateMachineEvent<State, State>>(
-  //   {
-  //     State: { change: "State" },
-  //   }, 
-  //   { key: "State", crossingRequested: false } 
-  // )
-
   const sharedStates = defineStates({
       State: (state: State = { key: "State", crossingRequested: false }) => state,
   })
+
   const sharedState = createMachine(
     sharedStates,
     {
@@ -93,7 +61,6 @@ export const createExtendedTrafficLightMachine = () => {
   const walkDuration = 
     states.Green().data.duration +
     states.Yellow().data.duration
-
     
   const greenWalkWarnAt = walkDuration - states.Yellow().data.duration - states.Green().data.duration / 2;
   const yellowWalkWarnAt = 0 //states.Yellow().data.duration;
@@ -117,10 +84,7 @@ export const createExtendedTrafficLightMachine = () => {
     {
       data: sharedState,
       requestCrossing: () => {
-        // console.log('request crossing')
-        sharedState.send("change", {
-          crossingRequested: true,
-        })
+        sharedState.send("change", { crossingRequested: true })
         machine.send("crossingRequested")
       },
     },
@@ -128,12 +92,8 @@ export const createExtendedTrafficLightMachine = () => {
   let timer: NodeJS.Timeout | null = null;
   let walkWarnTimer: NodeJS.Timeout | null = null;
   setup(machine)(
-    // enter(whenState("Green", (ev) => {
-      
-    // })),
     enter(whenState("Red", (ev) => {      
       const state = machine.data.getState();
-      // console.log('machine state', state)
       if (state.data.crossingRequested) {
         queueMicrotask(machine.api.crossingRequested)        
       }
@@ -144,59 +104,31 @@ export const createExtendedTrafficLightMachine = () => {
       })
     })),
     enter(ev => {
-      console.log("Entering state:", ev.to.key);
       if (walkWarnTimer) {
         clearTimeout(walkWarnTimer);
         walkWarnTimer = null;
       }
-      // if (ev.to.key !== "Yellow") {
-      //   if (sharedState.getState().data.walkWarningDuration) {
-      //     sharedState.send("change", {
-      //       walkWarningDuration: 0,
-      //     })
-      //   }
-      // } 
-      // // if (ev.to.key === "Green") {
-      // //   walkWarnTimer = setTimeout(() => {
-      // //     sharedState.send("change", {
-      // //       walkWarningDuration,
-      // //     })
-      // //   }, walkWarnAt)
-      // // } 
 
       // update pedestrian timer
       if (ev.to.is("Red") ) {
         sharedState.send("change", {
           walkWarningDuration: 0,
         })
-        // if (walkWarnTimer) {
-        //   clearTimeout(walkWarnTimer);
-        //   walkWarnTimer = null;
-        // }
       } else {
         const walkWarnAt = 
           ev.to.is("Green") ? greenWalkWarnAt : 
           ev.to.is("Yellow") ? yellowWalkWarnAt : -1;
-        console.log("walkWarnAt", walkWarnAt, "for state:", ev.to.key);
         if (walkWarnAt > -1) {
           const walkWarningDuration = 
             ev.to.is("Green") ? walkDuration - greenWalkWarnAt :
             ev.to.is("Yellow") ? states.Yellow().data.duration - yellowWalkWarnAt : 0;
-          console.log("Setting walk warning duration for state:", ev.to.key, "for duration:", walkWarningDuration);
           walkWarnTimer = setTimeout(() => {
-            // console.log("Setting walk warning duration for state:", ev.to.key, "for duration:", walkWarnAt);
-            sharedState.send("change", {
-              walkWarningDuration
-            })
+            sharedState.send("change", { walkWarningDuration })
           }, walkWarnAt);
         }
-        // sharedState.send("change", {
-        //   walkWarningDuration,
-        // })
       }
 
       // update timer
-
       if (timer) {
         clearTimeout(timer);
         timer = null;
@@ -205,15 +137,9 @@ export const createExtendedTrafficLightMachine = () => {
       if (duration === 0) {
         return
       }
-      // console.log("Setting timer for state:", ev.to.key, "for duration:", duration);
       timer = setTimeout(() => {
-        // console.log("Auto-transitioning from:", ev.to.key);
         machine.api.next();
       }, duration);
-      // return () => {
-      //   console.log("Exiting state:", ev.from.key);        
-      //   clearTimeout(timer);
-      // }
     })
   )  
   // start it
