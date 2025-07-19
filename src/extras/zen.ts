104125fbde453584d6bc44cb6776e60602bde763
Winston Fassett <winston.fassett@gmail.com>
2025-07-19 11:37:45 -0500

    tweak zen

--- src/extras/zen.ts
+++ src/extras/zen.ts
@@ -1,20 +1,23 @@
 import { setup } from "../ext";
-import { FactoryMachine, FactoryMachineContext } from "../factory-machine";
+import { FactoryMachine } from "../factory-machine";
 import { createApi } from "../factory-machine-event-api";
 
 export function zen<M extends FactoryMachine<any>>(machine: M) {
   const api = createApi(machine);
-  return {
-    ...api,
+  const zenMachine = Object.assign(machine, api, {
     get state(): ReturnType<M["getState"]> {
       return machine.getState();
     },
     get change(): ReturnType<M["getChange"]> {
       return machine.getChange() as any;
     },
-    get machine() {
-      return machine;
-    },
+    // get machine() {
+    //   return machine;
+    // },
     setup: setup(machine),
-  };
+  });
+  return zenMachine;
+  //  as M & typeof api;
 }
+export type ZenMachine<M extends FactoryMachine<any>> = M &
+  ReturnType<typeof createApi<M>>;
