303d77fd87547a251c234e1c6e4bf1964e4331b2
Winston Fassett <winston.fassett@gmail.com>
2025-07-18 22:55:01 -0500

    codetabs 1

--- docs/src/code/examples/stopwatch-using-react-state-using-lifecycle-instead-of-useEffect/index.tsx
+++ docs/src/code/examples/stopwatch-using-react-state-using-lifecycle-instead-of-useEffect/index.tsx
@@ -1,16 +1,16 @@
 
 import { MachineExampleWithChart } from "@components/MachineExampleWithChart";
-import { StopwatchApp } from "../components/StopwatchApp";
+import { StopwatchView } from "./StopwatchView";
 import { useStopwatch } from "./useStopwatch";
 
 // Main export for importing in MDX documentation
 export default function StopwatchExample() {
   const stopwatch = useStopwatch();
-  return <MachineExampleWithChart machine={stopwatch} AppView={StopwatchApp} showRawState={true} />;
+  return <MachineExampleWithChart machine={stopwatch} AppView={StopwatchView} showRawState={true} />;
 }
 
 // Named export for backward compatibility
 export function Stopwatch() {
   const stopwatch = useStopwatch();
-  return <StopwatchApp machine={stopwatch} />;
+  return <StopwatchView machine={stopwatch} />;
 }
