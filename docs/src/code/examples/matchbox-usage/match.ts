acd6fbb6f8e4709c12b61cc5c658642a6a77a3e4
Winston Fassett <winston.fassett@gmail.com>
2025-07-22 22:20:52 -0500

    pattern matching

---
+++ docs/src/code/examples/matchbox-usage/match.ts
@@ -0,0 +1,21 @@
+import { matchboxFactory } from "matchina";
+
+const shapes = matchboxFactory(
+  {
+    Circle: (radius: number) => ({ radius }),
+    Square: (side: number) => ({ side }),
+    Rectangle: (width: number, height: number) => ({ width, height }),
+  },
+  "type",
+);
+
+type Shape = ReturnType<(typeof shapes)[keyof typeof shapes]>;
+
+const someShape: Shape = shapes.Circle(5);
+
+// ---cut---
+const area = someShape.match({
+  Circle: ({ radius }) => Math.PI * radius * radius,
+  Square: ({ side }) => side * side,
+  Rectangle: ({ width, height }) => width * height,
+});
