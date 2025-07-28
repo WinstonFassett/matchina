5c5e391cdc1dd1481419ebb94b617f2e02fb3282
Winston Fassett <winston.fassett@gmail.com>
2025-07-28 09:26:09 -0500

    exclude util types

--- src/index.ts
+++ src/index.ts
@@ -16,7 +16,7 @@ export * from "./match-case-types";
 export * from "./factory-machine-event";
 export * from "./factory-machine-types";
 export * from "./state";
-export * from "./utility-types";
+// export * from "./utility-types";
 export * from "./matchbox-factory";
 export * from "./matchina";
 export * from "./extras/nanosubscriber";
