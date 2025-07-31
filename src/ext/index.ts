73a9e664cab1f083edccb3e2d02eb54ecc3fd5f7
Winston Fassett <winston.fassett@gmail.com>
2025-07-31 07:50:50 -0500

    export

--- src/ext/index.ts
+++ src/ext/index.ts
@@ -2,6 +2,7 @@ export * from "./funcware/abortable";
 export * from "./funcware/iff";
 export * from "./funcware/tap";
 export * from "./funcware/from-middleware";
+export * from "./methodware/enhance-function";
 export * from "./methodware/enhance-method";
 export * from "./methodware/method-enhancer";
 export * from "./setup";
