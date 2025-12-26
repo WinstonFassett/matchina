eb0c47377cccf196f0f21d909126bfcb8d69e119
Winston Fassett <dev@winstonfassett.com>
2025-12-26 08:28:54 -0600

    fix: complete buildDefinition function in XState converter

--- docs/src/code/examples/lib/matchina-machine-to-xstate-definition.ts
+++ docs/src/code/examples/lib/matchina-machine-to-xstate-definition.ts
@@ -27,25 +27,36 @@ export function getXStateDefinition<
       initial: declaredInitial,
       states: {} as Record<string, any>,
     };
-  }
-  // Object.entries(machine.states).forEach(([key, state]) => {
-  //   definition.states[key] = {
-  //     ...state,
-  //     on: {} as any,
-  //   }
-  // })
-  Object.entries(machine.transitions as object).forEach(
-    ([from, stateEvents]) => {
-      Object.entries(stateEvents as object).forEach(([event, entry]) => {
-        definition.states[from].on[event] = resolveState(
-          machine.states,
-          from,
-          entry
-        ).key;
-      });
+
+    // Initialize states
+    const stateValues = getStateValues(machine.states);
+    for (const state of stateValues) {
+      definition.states[state.key] = {
+        on: {} as any,
+      };
     }
-  );
-  return definition;
+
+    // Object.entries(machine.states).forEach(([key, state]) => {
+    //   definition.states[key] = {
+    //     ...state,
+    //     on: {} as any,
+    //   }
+    // })
+    Object.entries(machine.transitions as object).forEach(
+      ([from, stateEvents]) => {
+        Object.entries(stateEvents as object).forEach(([event, entry]) => {
+          definition.states[from].on[event] = resolveState(
+            machine.states,
+            from,
+            entry
+          ).key;
+        });
+      }
+    );
+    return definition;
+  }
+
+  return buildDefinition(machine, parentKey, []);
 }
 
 /**
